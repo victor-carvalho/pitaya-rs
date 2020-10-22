4c3b06b1947d918f30f3f3bb779aa4cc4c2afba3
Leonardo Hahn <leonardo.hahn@tfgco.com>
2020-10-22 19:48:43 -0300

    feat: remove default metrics reporter impl
    This allows the user to pass its own implementation of
    metrics::Reporter. The metrics setting was also removed.

--- src/pitaya/src/constants.rs
+++ src/pitaya/src/constants.rs
@@ -3,7 +3,3 @@ use std::time::Duration;
 pub const DEFAULT_ENV_PREFIX: &str = "PITAYA";
 
 pub const DEFAULT_SHUTDOWN_TIMEOUT: Duration = Duration::from_secs(30);
-
-pub const DEFAULT_METRICS_URL: &str = "127.0.0.1:8000";
-pub const DEFAULT_METRICS_PATH: &str = "/metrics";
-pub const DEFAULT_METRICS_NAMESPACE: &str = "pitaya";
